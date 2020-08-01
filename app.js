9ee2f1b67174f4b21bf8ae264ae1ef3204af3513
Michael Christenson <mchristenson5891@gmail.com>
2020-07-31 23:05:38 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 3030;
+
+const indexRouter = require("./routes/index");
+const moviesRouter = require("./routes/movies");
+
+const app = express();
+
+// view engine setup
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/movies", moviesRouter);
+
+app.listen(PORT, (err) =>
+  console.log(`${err ? err : `running on PORT ${PORT}`}`)
+);
